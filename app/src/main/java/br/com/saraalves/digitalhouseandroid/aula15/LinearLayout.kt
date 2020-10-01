ef4badfb45b848854a8857500369a2b23e383f8a
Sara Alves <saraalves14@hotmail.com>
2020-09-30 22:00:20 -0300

    Exercícios referentes a aula 15 sobre Linear Layout

---
+++ app/src/main/java/br/com/saraalves/digitalhouseandroid/aula15/LinearLayout.kt
@@ -0,0 +1,12 @@
+package br.com.saraalves.digitalhouseandroid.aula15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.saraalves.digitalhouseandroid.R
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
