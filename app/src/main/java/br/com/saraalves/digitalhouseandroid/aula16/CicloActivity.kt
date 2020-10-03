2b8ac0d3907d69f5d13094fe1e38bc44931e12d3
Sara Alves <saraalves14@hotmail.com>
2020-10-02 21:14:03 -0300

    criado tela simples de login

---
+++ app/src/main/java/br/com/saraalves/digitalhouseandroid/aula16/CicloActivity.kt
@@ -0,0 +1,12 @@
+package br.com.saraalves.digitalhouseandroid.aula16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.saraalves.digitalhouseandroid.R
+
+class CicloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciclo_activity)
+    }
+}
