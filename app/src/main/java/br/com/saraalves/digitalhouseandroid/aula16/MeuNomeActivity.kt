f6919c5981e341c55cf28385e74821f586fcb0cb
Sara Alves <saraalves14@hotmail.com>
2020-10-04 16:47:33 -0300

    exercício 1 para casa "Meu nome"

---
+++ app/src/main/java/br/com/saraalves/digitalhouseandroid/aula16/MeuNomeActivity.kt
@@ -0,0 +1,19 @@
+package br.com.saraalves.digitalhouseandroid.aula16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.saraalves.digitalhouseandroid.R
+import kotlinx.android.synthetic.main.activity_meu_nome.*
+
+class MeuNomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meu_nome)
+
+        btnExibir.setOnClickListener {
+            val nome = edtMeuNome.text
+
+            txtNomeDigitado.text = nome
+        }
+    }
+}
